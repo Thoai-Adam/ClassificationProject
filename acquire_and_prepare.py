84790411e6e3234350e534661dfcf2b9f2204bdd
Thoai Adam Nguyen <nguyenphithoaihung@gmail.com>
2023-08-17 18:08:47 -0500

    EOD commit

--- acquire_and_prepare.py
+++ acquire_and_prepare.py
@@ -5,3 +5,20 @@ def drop_columns(df):
     columns_to_drop = ['Unnamed: 0', 'payment_type_id', 'internet_service_type_id', 'contract_type_id', 'customer_id']
     df = df.drop(columns=columns_to_drop)
     return df
+
+
+
+
+
+
+def change_dtype(df):
+    df.total_charges = df.total_charges.replace(' ', 0)
+    df.total_charges = df.total_charges.astype(float)
+    return df
+
+def impute_mode(train, validate, test):
+    imputer = SimpleImputer(fill_value= 'unknown', strategy='constant')
+    train[['churn']] = imputer.fit_transform(train[['churn']])
+    validate[['churn']] = imputer.transform(validate[['churn']])
+    test[['churn']] = imputer.transform(test[['churn']])
+    return train, validate, test
